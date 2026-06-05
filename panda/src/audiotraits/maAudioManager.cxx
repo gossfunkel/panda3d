8e0dfc61fb55f260920fa75ddfc6c3f4bd2a1555
gossfunkel <katherineegoss@gmail.com>
2026-06-05 01:50:47 +0100

    more bits and pieces for miniaudio resource manager in constructor

--- panda/src/audiotraits/maAudioManager.cxx
+++ panda/src/audiotraits/maAudioManager.cxx
@@ -10,26 +10,42 @@ MaAudioManager::
 MaAudioManager() {
   // lock mutex
 
-  ma_device_config device_config = ma_device_config_init(ma_device_type_playback);
-  config.playback.format = ma_format_f32;
-  config.playback.channels = 2;
-  config.samplerate = 48000;
-  //config.dataCallback
-  //config.pUserData
+  audio_cat.init();
 
-  check_ma(ma_device_init(NULL, &device_config, &_device), ,
-           "Failed to initialise MiniAudio device.");
+  if (_managers == nullptr) {
+    _managers = new Managers;
 
-  ma_device_start(&_device);
+    ma_device_config device_config = ma_device_config_init(ma_device_type_playback);
+    config.playback.format = ma_format_f32;
+    config.playback.channels = 2;
+    config.samplerate = 48000;
+    //config.dataCallback
+    //config.pUserData
 
-  //ma_resource_manager_config _resource_mgr_conf; TODO member variable
-  ma_resource_manager resource_mgr;
-  resource_mgr_conf = ma_resource_manager_config_init();
-  check_ma(
-    ma_resource_manager_init(&resource_mgr_conf, &resource_mgr),
-    ma_device_uninit(&_device),
-    "Failed to initialise MiniAudio resource manager."
-  );
+    check_ma(ma_device_init(NULL, &device_config, &_device), ,
+             "Failed to initialise MiniAudio device.");
+
+    ma_device_start(&_device);
+
+    audio_cat.info() << "Using MiniAudio device " << device.playback.name <<
+      "." << std::endl;
+
+    //ma_resource_manager_config _resource_mgr_conf; TODO member variable
+    //ma_resource_manager _resource_mgr; TODO member variable
+    resource_mgr_conf = ma_resource_manager_config_init();
+    resource_mgr_conf.decodedFormat = _device.playback.format;
+    resource_mgr_conf.decodedChannels = _device.playback.channels;
+    resource_mgr_conf.decodedSampleRate = _device.sampleRate;
+    //resource_mgr_conf.jobThreadCount = 2;
+    //resource_mgr_conf.pVFS = _vfs;
+    check_ma(
+      ma_resource_manager_init(&resource_mgr_conf, &_resource_mgr),
+      ma_device_uninit(&_device),
+      "Failed to initialise MiniAudio resource manager."
+    );
+  }
+
+  _managers->insert(this);
 
   //ma_engine _audio_engine; TODO member variable
   ma_engine_config audio_engine_conf;
