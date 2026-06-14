ac30cbb3b50622bfd1fe374fad6c9194331ab155
gossfunkel <katherineegoss@gmail.com>
2026-06-14 14:04:08 +0100

    update to draft for test_maAudioManager; fixed await, improved comments for clarity

--- tests/audio/test_maAudioManager.py
+++ tests/audio/test_maAudioManager.py
@@ -4,9 +4,10 @@ import pytest
 
 def test_create_shutdown():
     test_mgr = AudioManager.create_AudioManager()
+    # give the manager a moment to run its update method a few times
+    await(1)
     assert !(str(test_mgr).startswith("NullAudioManager"))
     test_mgr.shutdown()
-    # TODO does this work with python objects?
-    wait(1)
+    # TODO does this work with python objects - will test_mgr be None after destruction?
     assert test_mgr == None
 
