19d7042ed0380d9f5ec57fed95ad8aae66433b82
J0y <npflieger.chakma@gmail.com>
2026-06-21 22:55:59 +0200

    Fix issues in test_maAudioManager.py
    * replace `await()` by `sleep()`
    * replace `!` by `not`

--- tests/audio/test_maAudioManager.py
+++ tests/audio/test_maAudioManager.py
@@ -1,12 +1,15 @@
 import os
-
 import pytest
+from time import sleep
+
+from panda3d.core import AudioManager
 
 def test_create_shutdown():
     test_mgr = AudioManager.create_AudioManager()
     # give the manager a moment to run its update method a few times
-    await(1)
-    assert !(str(test_mgr).startswith("NullAudioManager"))
+    # await(1) # i don't think that functions exist in python
+    sleep(1)
+    assert not str(test_mgr).startswith("NullAudioManager")
     test_mgr.shutdown()
     # TODO does this work with python objects - will test_mgr be None after destruction?
     assert test_mgr == None
