e10d3edea5f30103df7162831eb0556d524f244b
gossfunkel <katherineegoss@gmail.com>
2026-06-24 15:58:46 +0100

    missing comment?

--- tests/audio/test_maAudioManager.py
+++ tests/audio/test_maAudioManager.py
@@ -5,6 +5,7 @@ import pytest
 def test_create_shutdown():
     test_mgr = AudioManager.create_AudioManager()
     # give the manager a moment to run its update method a few times
+    # TODO asyncio or sequence/interval?
     await(1)
     assert !(str(test_mgr).startswith("NullAudioManager"))
     test_mgr.shutdown()
