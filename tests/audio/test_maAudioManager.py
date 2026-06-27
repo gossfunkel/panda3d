2a509ed3196dc761b8d06ff2b9d91c7bac8312dc
J0y <npflieger.chakma@gmail.com>
2026-06-27 02:39:44 +0200

    refix test_maAudioManager.py after a merge conflict

--- tests/audio/test_maAudioManager.py
+++ tests/audio/test_maAudioManager.py
@@ -1,22 +1,18 @@
+import asyncio
 import os
 import pytest
-from time import sleep
 
 from panda3d.core import AudioManager
 
-def test_create_shutdown():
-    test_mgr = AudioManager.create_AudioManager()
-    # give the manager a moment to run its update method a few times
-<<<<<<< HEAD
-    # await(1) # i don't think that functions exist in python
-    sleep(1)
-    assert not str(test_mgr).startswith("NullAudioManager")
-=======
-    # TODO asyncio or sequence/interval?
-    await(1)
-    assert !(str(test_mgr).startswith("NullAudioManager"))
->>>>>>> origin/ma_backend
-    test_mgr.shutdown()
-    # TODO does this work with python objects - will test_mgr be None after destruction?
-    assert test_mgr == None
 
+async def test_create_shutdown(mgr):
+    # give the manager a moment to run its update method a few times
+    await asyncio.sleep(1)
+    assert not str(mgr).startswith("NullAudioManager")
+    mgr.shutdown()
+    # TODO does this work with python objects - will mgr be None after destruction?
+    assert mgr == None
+
+def test_audio_manager_creation():
+    test_mgr = AudioManager.create_AudioManager()
+    asyncio.run(test_create_shutdown(test_mgr))
